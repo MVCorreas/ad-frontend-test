098de9e89b6affbf47ff101965581a6a340f6937
MVCorreas <correasmv@gmail.com>
2025-07-19 17:15:45 -0300

    feat(pagination): follow number of pages after search filter

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -7,24 +7,23 @@ export async function GET(request: Request) {
   const genre = searchParams.get("genre");
   let page = parseInt(searchParams.get("page") ?? "1");
 
-  let games = allGames;
+  let filteredGames = allGames;
 
   if (genre) {
-    games = games.filter(
+    filteredGames = filteredGames.filter(
       (game) => game.genre.toLowerCase() === genre.toLowerCase()
     );
   }
 
   if (page < 1 || isNaN(page)) page = 1;
 
-  // Mock a delay to simulate a real API
   await delay(2000);
 
+  const totalPages = Math.ceil(filteredGames.length / ITEMS_PER_PAGE);
   const fromIndex = (page - 1) * ITEMS_PER_PAGE;
   const toIndex = page * ITEMS_PER_PAGE;
-  games = games.slice(fromIndex, toIndex);
+  const games = filteredGames.slice(fromIndex, toIndex);
 
-  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
   const currentPage = page;
 
   return Response.json({ games, availableFilters, totalPages, currentPage });

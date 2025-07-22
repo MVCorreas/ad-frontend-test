e7fb6f57ea56cca9b0b197fe0442faad330004b8
MVCorreas <correasmv@gmail.com>
2025-07-22 12:41:51 -0300

    style(catalogHeader): adapted figma styles to catalog header

--- src/components/CatalogHeader.tsx
+++ src/components/CatalogHeader.tsx
@@ -10,14 +10,15 @@ export default function CatalogHeader({
   return (
     <div className="border-b border-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-start">
           <div>
-            <h1 className="text-2xl font-bold text-colour-primary">Top Sellers</h1>
+            <h1 className="text-2xl font-bold text-colour-primary">
+              Top Sellers
+            </h1>
           </div>
-
-          <div className="mt-4 sm:mt-0">
-            <SearchBar availableFilters={availableFilters} />
-          </div>
+        </div>
+        <div className="mt-8 flex flex-col sm:flex-row sm:items-center sm:justify-end">
+          <SearchBar availableFilters={availableFilters} />
         </div>
       </div>
     </div>
